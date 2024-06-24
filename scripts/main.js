206f9e3d0e64d4b0d46c19101e031d4aeff523db
Judson Hartley <judson.hartley2@gmail.com>
2024-06-24 18:14:27 -0500

    add code comments

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-
+//Page Framework
 const title = "Assignment 2";
 const date = "20240624";
 
@@ -10,21 +10,27 @@ dateEl.innerHTML = date;
 
 const fireworksEl = document.getElementById("fireworks");
 const infoEl = document.getElementById("info");
+
+//clear any message boxes
 function clearMsg(x){
   console.log(x);
   fireworksEl.style.display = 'none';
   infoEl.style.display = 'none';
 }
 
+//show the infoBox modal
 const infoTextEl = document.getElementById("infoText");
 function infoBox(msg){
   infoEl.style.display = 'block';
   infoTextEl.innerText = msg;
 }
 
+//get a random integer from 1 to 10
 function getRandomInt(){
   return Math.floor((Math.random() * 10)+1);
 }
+
+//event driven function for item the while loop.
 let jsGuess = getRandomInt();
 function newGuess(guess){
   console.log(`Guess is ${guess} number is ${jsGuess}`);
@@ -34,22 +40,21 @@ function newGuess(guess){
   }
 }
 
-let pw1value = "";
-let pw2value = "";
-
-function checkPw(){
-  pw1value = pw1.value;
-  pw2value = pw2.value;
-  console.log(`pw1 ${pw1value} pw2 ${pw2value}`);
-  if (pw1value.length < 4) return infoBox("First enter a password more than four characters");
-  if (pw2.value == "") return;
-  if (pw2value.length < 4 || pw1value != pw2value) return infoBox("Enter the same password in the Verify box");
-  fireworksEl.style.display = 'block';
-}
-
 const pw1 = document.getElementById("pw1");
 const pw2 = document.getElementById("pw2");
 const showPw = document.getElementById("showPw");
+
+function checkPw(){ //this is event driven, no actual need for any loop... The question would like a do while loop...
+  do {
+    console.log(`pw1 ${pw1.value} pw2 ${pw2.value}`);
+    if (pw1.value.length < 4) return infoBox("First enter a password more than four characters");
+    if (pw2.value == "") return;
+    if (pw2.value.length < 4 || pw1.value != pw2.value) return infoBox("Enter the same password in the Verify box");
+    fireworksEl.style.display = 'block';
+  } while (false);
+}
+
+// Function to toggle password visibility
 let pwVis = false;
 function togglePwVis(){
   pwVis = !pwVis;
@@ -64,6 +69,7 @@ function togglePwVis(){
   }
 }
 
+//function to create multiplication table and add to the DOM
 const mulTable = document.getElementById("mulTable");
 function makeTable(value){
   mulTable.innerHTML = '';
